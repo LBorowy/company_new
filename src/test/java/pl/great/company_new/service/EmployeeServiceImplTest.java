e4d30939627356e9ce6c50dcf489f34fa60b11d1
Boro <lukasz.borowy@satlan.pl>
2022-09-07 22:01:07 +0200

    Added mockito and tests for service layer

---
+++ src/test/java/pl/great/company_new/service/EmployeeServiceImplTest.java
@@ -0,0 +1,89 @@
+package pl.great.company_new.service;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+import pl.great.company_new.dto.EmployeeDto;
+import pl.great.company_new.entity.Employee;
+import pl.great.company_new.repository.EmployeeRepositoryImpl;
+
+import java.math.BigDecimal;
+
+import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.Mockito.*;
+
+@ExtendWith(MockitoExtension.class)
+class EmployeeServiceImplTest {
+
+    private static final String FIRST_NAME = "FIRST_NAME";
+    private static final String LAST_NAME = "LAST_NAME";
+    private static final String PESEL = "PESEL";
+    private static final BigDecimal SALARY = BigDecimal.TEN;
+
+    private Employee employee;
+    private EmployeeDto employeeDto;
+
+    @Mock
+    EmployeeRepositoryImpl employeeRepository;
+    @InjectMocks
+    EmployeeServiceImpl employeeService;
+
+    @BeforeEach
+    void setUp() {
+        employee = new Employee(FIRST_NAME, LAST_NAME, PESEL, SALARY);
+        employeeDto = new EmployeeDto(FIRST_NAME, LAST_NAME, PESEL, SALARY);
+    }
+
+    @Test
+    void get() {
+        when(employeeRepository.get(PESEL)).thenReturn(employee);
+
+        EmployeeDto employeeDto = employeeService.get(PESEL);
+
+        assertNotNull(employeeDto);
+        assertEquals(FIRST_NAME, employeeDto.getFirstName());
+        assertEquals(LAST_NAME, employeeDto.getLastName());
+        assertEquals(PESEL, employeeDto.getPesel());
+        assertEquals(SALARY, employeeDto.getSalary());
+    }
+
+    @Test
+    void create() throws Exception {
+        when(employeeRepository.create(anyString(), anyString(), anyString(), any())).thenReturn(employee);
+
+        EmployeeDto employeeDto = employeeService.create(FIRST_NAME, LAST_NAME, PESEL, SALARY);
+
+        assertNotNull(employeeDto);
+        assertEquals(FIRST_NAME, employeeDto.getFirstName());
+        assertEquals(LAST_NAME, employeeDto.getLastName());
+        assertEquals(PESEL, employeeDto.getPesel());
+        assertEquals(SALARY, employeeDto.getSalary());
+    }
+
+    @Test
+    void update() {
+        when(employeeRepository.get(any(String.class))).thenReturn(employee);
+        when(employeeRepository.update(any(Employee.class))).thenReturn(employee);
+
+        EmployeeDto updatedEmployeeDto = employeeService.update(employeeDto);
+
+        assertNotNull(updatedEmployeeDto);
+        assertEquals(FIRST_NAME, updatedEmployeeDto.getFirstName());
+        assertEquals(LAST_NAME, updatedEmployeeDto.getLastName());
+        assertEquals(PESEL, updatedEmployeeDto.getPesel());
+        assertEquals(SALARY, updatedEmployeeDto.getSalary());
+    }
+
+    @Test
+    void delete() {
+        when(employeeRepository.get(PESEL)).thenReturn(employee);
+        when(employeeRepository.delete(PESEL)).thenReturn(Boolean.TRUE);
+
+        boolean isEmployeeDeleted = employeeService.delete(PESEL);
+
+        assertTrue(isEmployeeDeleted);
+    }
+}
